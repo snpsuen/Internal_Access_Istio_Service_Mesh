31ac3d7d49ea1b70bb6659571974a0afc74af049
snpsuen <66404197+snpsuen@users.noreply.github.com>
2022-12-16 15:38:15 +0800

    Update app.py

--- meshfront/app.py
+++ meshfront/app.py
@@ -1,25 +1,13 @@
-import tensorflow as tf
-from tensorflow import keras
-from numpy import asarray
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
-model = tf.keras.models.load_model("./rnn_sales_timeseries_model")
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/prediction', methods=['GET', 'POST'])
-def prediction():
+@app.route('/accessmesh', methods=['GET', 'POST'])
+def accessmesh():
   if request.method == 'POST':
-    n_steps = 5
-    past=[]
-    for i in range(1, n_steps + 1):
-      month = "month0" + str(i)
-      result = float(request.form.get(month))
-      past.append(result)
-
-    row = asarray(past).reshape((1, n_steps, 1))
-    yhat = model.predict(row)
-    return render_template('result.html', result=yhat)
+    path = request.form['path']
+    return redirect(f"http://service-mesh/{path}")
